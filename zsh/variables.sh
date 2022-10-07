f397e83b121253e38c4827134c220f5ae8ff56c7
Samuel Naranjo <39077795+snaraj@users.noreply.github.com>
2022-10-07 00:34:35 -0500

    Update variables.sh

--- zsh/variables.sh
+++ zsh/variables.sh
@@ -1,4 +1,4 @@
-export CARGO_HOME="$HOME/.cargo"
 export DOTFILES="$HOME/.config"
-export BAT_CONFIG_PATH="$DOTFILES/bat/bat.conf"
 
+export CARGO_HOME="$HOME/.cargo"
+export BAT_CONFIG_PATH="$DOTFILES/bat/bat.conf"
